aad145cc35791c4e9f52f628146579ce4ace2eb7
95ktsmith <95ktsmith@gmail.com>
2020-09-27 20:04:33 +0000

    Updating.

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,21 +1,22 @@
-$(document).ready(function () {
-  $('INPUT[type=checkbox]').change(function(){
-      let checked = {};
-      if($(this).is(':checked')) {
-        checked[$(this).attr('data-id')] = $(this).attr('data-name');
-          // Checkbox is checked.. update h4 in DIV amenities
-      } else {
-        delete checked[$(this).attr('data-id')];
-        // Checkbox is not checked..
-      }
-      let subhead = "";
-      for (var key in checked) {
-        if (subhead != "") {
-          subhead = subhead.concat(", ");
+  $(document).ready(function () {
+  let checked = {};
+  for (const checkbox in $('INPUT[type=checkbox]')) {
+      $('INPUT[type=checkbox]').change(function(){
+        if($(this).is(':checked')) {
+          checked[$(this).attr('data-id')] = $(this).attr('data-name');
+            // Checkbox is checked.. update h4 in DIV amenities
+        } else {
+          delete checked[$(this).attr('data-id')];
+          // Checkbox is not checked..
         }
-          subhead = subhead.concat(checked[key]);
-      }
-      $(".amenities h4").text(subhead);
-  });
+        let subhead = "";
+        for (var key in checked) {
+          if (subhead != "") {
+            subhead = subhead.concat(", ");
+          }
+            subhead = subhead.concat(checked[key]);
+        }
+        $(".amenities h4").text(subhead);
+    });
+  }
 });
-
