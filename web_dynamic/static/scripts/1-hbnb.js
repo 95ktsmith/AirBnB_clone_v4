1ea9685140ae8639aa144bd0619f6b9b38b7b2be
95ktsmith <95ktsmith@gmail.com>
2020-09-27 20:41:58 +0000

    Fixing for semistandard.

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,22 +1,20 @@
-  $(document).ready(function () {
-  let checked = {};
-  for (const checkbox in $('INPUT[type=checkbox]')) {
-      $('INPUT[type=checkbox]').change(function(){
-        if($(this).is(':checked')) {
-          checked[$(this).attr('data-id')] = $(this).attr('data-name');
-            // Checkbox is checked.. update h4 in DIV amenities
-        } else {
-          delete checked[$(this).attr('data-id')];
-          // Checkbox is not checked..
-        }
-        let subhead = "";
-        for (var key in checked) {
-          if (subhead != "") {
-            subhead = subhead.concat(", ");
-          }
-            subhead = subhead.concat(checked[key]);
-        }
-        $(".amenities h4").text(subhead);
+const $ = window.$;
+$(document).ready(function () {
+  const checked = {};
+  for (let box = 0; box < $('INPUT[type=checkbox]').length; box++) {
+    $('INPUT[type=checkbox]').change(function () {
+      if ($(this).is(':checked')) {
+        checked[$(this).attr('data-id')] = $(this).attr('data-name');
+        // Checkbox is checked.. update h4 in DIV amenities
+      } else {
+        delete checked[$(this).attr('data-id')];
+        // Checkbox is not checked..
+      }
+      const subhead = [];
+      for (const key in checked) {
+        subhead.push(checked[key]);
+      }
+      $('.amenities h4').text(subhead.join(', '));
     });
   }
 });
