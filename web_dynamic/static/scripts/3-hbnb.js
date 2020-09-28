a18702fba52c1e333c90d8359d184f0b44a51e97
95ktsmith <95ktsmith@gmail.com>
2020-09-28 20:07:40 +0000

    Changed from localhost to 0.0.0.0 in checkapistatus.

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -52,7 +52,7 @@ function checkAmenities () {
 }
 
 function checkApiStatus () {
-  const url = 'http://localhost:5001/api/v1/status/';
+  const url = 'http://0.0.0.0:5001/api/v1/status/';
   $.get(url, function (data) {
     if (data.status === 'OK') {
       $('#api_status').addClass('available');
