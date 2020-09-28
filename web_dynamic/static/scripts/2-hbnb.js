8185613ba52d95b365a857c29852ce969e57b149
95ktsmith <95ktsmith@gmail.com>
2020-09-28 15:46:37 +0000

    Changed localhost url to 0.0.0.0 for api status.

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
     });
   }
 
-  const url = 'http://localhost:5001/api/v1/status/';
+  const url = 'http://0.0.0.0:5001/api/v1/status/';
   $.get(url, function (data) {
     if (data.status === 'OK') {
       $('#api_status').addClass('available');
